8be1701f280e3393116a57a49a4cf69d8b585e7e
Alexey Kiselev <akiselev@elvees.com>
2016-09-29 23:37:37 +0300

    mcom: Remove configuration defines for SBCDBG board
    Configuration defines for SBCDBG board are merged into mcom.h.
    
    Change-Id: I4345477b9099ff66a9599b2c8a9544a7218456e2

--- include/configs/mcom.h
+++ include/configs/mcom.h
@@ -119,6 +119,107 @@
 
 #define CONFIG_FAT_WRITE    /* enable write access */
 
+#define CONFIG_NR_DRAM_BANKS		2
+#define PHYS_SDRAM_0			CONFIG_SYS_SDRAM_BASE
+#define PHYS_SDRAM_0_SIZE		(CONFIG_DDR_SIZE_IN_MB << 20)
+#define PHYS_SDRAM_1			0xa0000000
+#define PHYS_SDRAM_1_SIZE		(CONFIG_DDR_SIZE_IN_MB << 20)
+
+#define CONFIG_ENV_IS_IN_MMC
+#define CONFIG_SYS_MMC_ENV_DEV		0   /* first detected MMC controller */
+
+#define CONFIG_SF_DEFAULT_SPEED		33000000
+#define CONFIG_CMD_SF_TEST
+
+#if !defined CONFIG_ENV_IS_IN_MMC && \
+	!defined CONFIG_ENV_IS_IN_NAND && \
+	!defined CONFIG_ENV_IS_IN_FAT && \
+	!defined CONFIG_ENV_IS_IN_SPI_FLASH
+#define CONFIG_ENV_IS_NOWHERE
+#endif
+
+#define CONFIG_SYS_CONSOLE_IS_IN_ENV
+
+#ifndef CONFIG_SPL_BUILD
+#include <config_distro_defaults.h>
+
+/* Enable pre-console buffer to get complete log on the VGA console */
+#define CONFIG_PRE_CONSOLE_BUFFER
+#define CONFIG_PRE_CON_BUF_SZ		1024
+/* Use the room between the end of bootm_size and the framebuffer */
+#define CONFIG_PRE_CON_BUF_ADDR		0x4f000000
+
+/* @fixme We should use bootm instead of bootz. */
+/* @fixme Moving fdt is very ugly. There should be way to do it properly. */
+#define CONFIG_BOOTCOMMAND \
+	"echo 'Loading Linux...'; " \
+	"load mmc 0:1 ${env_addr_r} u-boot.env; " \
+	"size mmc 0:1 u-boot.env; " \
+	"env import -t ${env_addr_r} ${filesize}; " \
+	"load mmc 0:1 ${kernel_addr_r} zImage; " \
+	"fdt addr ${fdtcontroladdr}; " \
+	"fdt move ${fdtcontroladdr} ${fdt_addr_r} 0x8000; " \
+	"fdt addr ${fdt_addr_r}; " \
+	"if test ${ddrctl_cmd} = 'disable'; then " \
+		"ddrctl ${ddrctl_cmd} ${ddrctl_cid}; fi; " \
+	"bootz ${kernel_addr_r} - ${fdt_addr_r}"
+
+#define MEM_LAYOUT_ENV_SETTINGS \
+	"bootm_size=0xf000000\0" \
+	"kernel_addr_r=0x40008000\0" \
+	"loadaddr=0x40008000\0" \
+	"fdtaddr=0x50000000\0" \
+	"fdt_addr_r=0x50000000\0" \
+	"mmcdev=0\0" \
+	"mmcroot=/dev/mmcblk0p1 rw\0" \
+	"mmcrootfstype=ext4 rootwait\0" \
+	"root=/dev/mmcblk0p1\0" \
+	"scriptaddr=0x43100000\0" \
+	"pxefile_addr_r=0x43200000\0" \
+	"ramdisk_addr_r=0x43300000\0" \
+	"env_addr_r=0x45000000\0" \
+	"ddrctl_cid=1\0" \
+	"ddrctl_cmd=disable\0"
+
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0)
+
+#include <config_distro_bootcmd.h>
+
+#ifdef CONFIG_USB_KEYBOARD
+#define CONSOLE_STDIN_SETTINGS \
+	"preboot=usb start\0" \
+	"stdin=serial,usbkbd\0"
+#else
+#define CONSOLE_STDIN_SETTINGS \
+	"stdin=serial\0"
+#endif
+
+#ifdef CONFIG_VIDEO
+#define CONSOLE_STDOUT_SETTINGS \
+	"stdout=serial,vga\0" \
+	"stderr=serial,vga\0"
+#else
+#define CONSOLE_STDOUT_SETTINGS \
+	"stdout=serial\0" \
+	"stderr=serial\0"
+#endif
+
+#define CONSOLE_ENV_SETTINGS \
+	CONSOLE_STDIN_SETTINGS \
+	CONSOLE_STDOUT_SETTINGS
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	CONSOLE_ENV_SETTINGS \
+	MEM_LAYOUT_ENV_SETTINGS \
+	"fdtfile=" CONFIG_FDTFILE "\0" \
+	"console=ttyS0,115200\0" \
+	BOOTENV
+
+#else /* ifndef CONFIG_SPL_BUILD */
+#define CONFIG_EXTRA_ENV_SETTINGS
+#endif
+
 /* SPL framework */
 #define CONFIG_SPL_FRAMEWORK
 
