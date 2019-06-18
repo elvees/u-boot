f18891f85d93779fc2392ab8452d43c356d9bf3d
Timur Prokopenko <tprokopenko@elvees.com>
2019-06-18 20:07:52 +0300

    mcom03: Add initial MCom-03 and HAPS support
    This is the minimal support required to boot U-Boot
    
    Issue: #MCOM03SW-128
    Change-Id: Ibbd6ee87b35585d6b9d9e942eb5239c5dd9774a9

---
+++ include/configs/mcom03-haps.h
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2019 RnD Center "ELVEES", JSC
+ *
+ * Configuration settings for the MCom-03 platform
+ */
+
+#ifndef __MCOM_03_HAPS_H
+#define __MCOM_03_HAPS_H
+
+#include <linux/sizes.h>
+
+#define CONFIG_SYS_SDRAM_BASE		0xC0000000
+#define PHYS_SDRAM_0			CONFIG_SYS_SDRAM_BASE
+#define PHYS_SDRAM_0_SIZE		SZ_256M
+#define CONFIG_SYS_MALLOC_LEN		SZ_8M
+
+#define CONFIG_SYS_INIT_SP_ADDR		0xC0400000
+
+#define CONFIG_SYS_LOAD_ADDR		0xC2000000 /* default load address */
+
+/* standalone support */
+#define CONFIG_STANDALONE_LOAD_ADDR	0xC2000000
+
+/* Default environment */
+#define CONFIG_BOOTFILE			"Image"
+#define CONFIG_LOADADDR			0xC0080000
+
+#define CONFIG_BOOTCOMMAND \
+	"booti"
+
+#endif
