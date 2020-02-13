c5cb51b8959e8df1b5189450f2b520e9075ed123
Dmitriy Zagrebin <dzagrebin@elvees.com>
2020-02-13 14:45:09 +0300

    mcom03: Create empty gpio.h
    Drivers that uses GPIO require platform specific
    asm/arch-<platofrm>/gpio.h header to exist.
    This commit creates empty gpio.h for MCom-03 platform.
    
    Change-Id: I1caf7237f11302da7b8924e83cf9f7fdf35afaee

---
+++ arch/arm/include/asm/arch-mcom03/gpio.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2020 RnD Center "ELVEES", JSC
+ */
+
+#ifndef _MCOM03_GPIO_H
+#define _MCOM03_GPIO_H
+
+/* Empty file - cmd_gpio.c requires this. */
+
+#endif
