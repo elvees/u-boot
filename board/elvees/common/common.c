e3c2ac44b4b0c09ed613e2eea30cb8332395e120
Vasiliy Zasukhin <vzasukhin@elvees.com>
2021-07-23 20:23:24 +0300

    mcom03: Add pads setup
    This is a temporary solution, pinctrl driver will
    do it in future.
    
    Issue: #MCOM03BUP-70
    Change-Id: I181618eabf44c8760216efdd75a86a45542b97fb

--- board/elvees/common/common.c
+++ board/elvees/common/common.c
@@ -6,12 +6,17 @@
 #include <common.h>
 #include <asm/armv8/mmu.h>
 #include <asm/io.h>
+#include <linux/bitops.h>
 #include <linux/kernel.h>
 
 #include "mcom03-clk.h"
 
 #define DDR_SUBS_URB_BASE		0xC000000
 #define HSPERIPH_BAR			0xDC
+
+#define GPIO1_PORTA_PAD_CTR(x)		(0x17e0020 + (x) * 0x4)
+#define LSP1_GPIO_SWPORTA_CTL		0x1780008
+#define GPIO_PAD_CTR_EN			BIT(12)
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -47,12 +52,51 @@ int dram_init(void)
 	return 0;
 }
 
+static void setup_pads(void)
+{
+	u32 val;
+
+	/* Used for I2C1_SCL */
+	val = readl(GPIO1_PORTA_PAD_CTR(0));
+	val |= GPIO_PAD_CTR_EN;
+	writel(val, GPIO1_PORTA_PAD_CTR(0));
+
+	/* Used for I2C1_DAT */
+	val = readl(GPIO1_PORTA_PAD_CTR(1));
+	val |= GPIO_PAD_CTR_EN;
+	writel(val, GPIO1_PORTA_PAD_CTR(1));
+
+	/* Used for I2C2_SCL */
+	val = readl(GPIO1_PORTA_PAD_CTR(2));
+	val |= GPIO_PAD_CTR_EN;
+	writel(val, GPIO1_PORTA_PAD_CTR(2));
+
+	/* Used for I2C2_DAT */
+	val = readl(GPIO1_PORTA_PAD_CTR(3));
+	val |= GPIO_PAD_CTR_EN;
+	writel(val, GPIO1_PORTA_PAD_CTR(3));
+
+	/* Used for I2C3_SCL */
+	val = readl(GPIO1_PORTA_PAD_CTR(4));
+	val |= GPIO_PAD_CTR_EN;
+	writel(val, GPIO1_PORTA_PAD_CTR(4));
+
+	/* Used for I2C3_DAT */
+	val = readl(GPIO1_PORTA_PAD_CTR(5));
+	val |= GPIO_PAD_CTR_EN;
+	writel(val, GPIO1_PORTA_PAD_CTR(5));
+
+	/* Turn GPIO1_A0 ... GPIO1_A5 to hardware mode (I2C1, I2C2, I2C3) */
+	writel(0x3f, LSP1_GPIO_SWPORTA_CTL);
+}
+
 int board_init(void)
 {
 	/* DDR high memory range is not available on HAPS. It means that
 	 * GEMAC DMA has access to DDR only if HSPERIPH_BAR is set to 0.
 	 */
 	writel(0, DDR_SUBS_URB_BASE + HSPERIPH_BAR);
+	setup_pads();
 
 	return clk_cfg();
 }
