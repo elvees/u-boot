ef168e900500e0f36c25243ab015b476e14da6b6
Dmitriy Zagrebin <dzagrebin@elvees.com>
2021-10-04 12:54:13 +0300

    mcom03: Add ECAM03DM board support
    Issue: #MCOM03SW-778
    Change-Id: I54d658986c435c8deb8cc392e7d1f60a41810464

---
+++ board/elvees/ecam03dm/ecam03dm.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2021 RnD Center "ELVEES", JSC
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <linux/kernel.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int dram_init_banksize(void)
+{
+	memcpy(gd->bd->bi_dram, (void *)CONFIG_MEM_REGIONS_ADDR,
+	       sizeof(struct bd_info));
+
+	return 0;
+}
+
+void board_pads_cfg(void)
+{
+}
