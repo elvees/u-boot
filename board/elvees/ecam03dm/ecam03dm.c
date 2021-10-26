8d55de499d788b96c8c50b98c7712cf1af47d95e
Oleg Ivanov <oaivanov@elvees.com>
2021-10-26 16:09:54 +0300

    mcom03: Enable pad for Ethernet PHY interrupt line on ECAM03DM
    Configure the required pad to use as Ethernet PHY interrupt line
    in Linux on ECAM03DM board.
    
    Issue: #MCOM03SW-807
    Change-Id: Ide89983c5b69131ab133e9b84c884cdc320200f2

--- board/elvees/ecam03dm/ecam03dm.c
+++ board/elvees/ecam03dm/ecam03dm.c
@@ -7,8 +7,19 @@
 #include <asm/io.h>
 #include <linux/kernel.h>
 
+#include "../common/mcom03-common.h"
+
 DECLARE_GLOBAL_DATA_PTR;
 
 void board_pads_cfg(void)
 {
+	u32 val;
+
+	/* Enable receivers for GPIO1_A6,
+	 * which are used as Ethernet PHY interrupt.
+	 * Temporary code until support is added to pinctrl
+	 */
+	val = readl(GPIO1_PORTA_PAD_CTR(6));
+	val |= GPIO_PAD_CTR_EN;
+	writel(val, GPIO1_PORTA_PAD_CTR(6));
 }
