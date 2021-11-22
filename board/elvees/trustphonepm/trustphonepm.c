ad489849980e713d625c3006baf824b993d56606
Dmitriy Zagrebin <dzagrebin@elvees.com>
2021-11-22 13:33:58 +0300

    board: elvees: trustphonepm: Turn on LTE module
    According to specification Quectel EG95-EX LTE module
    requires a pulse on PWRKEY input pin to turn on. Pulse
    duration must be at least 500ms.
    
    This commit is a temporary solution. LTE module turning on
    should be done by userspace.
    
    Issue: #MCOM03SW-880
    Issue: #MCOM03SW-1036
    Change-Id: I17b81a775f1a68263495e015b9c5327827b1d558

--- board/elvees/trustphonepm/trustphonepm.c
+++ board/elvees/trustphonepm/trustphonepm.c
@@ -22,3 +22,28 @@ void board_pads_cfg(void)
 	for (i = 0; i < 4; i++)
 		i2c_pad_cfg(i);
 }
+
+int power_init_board(void)
+{
+	u32 val;
+
+	/* LTE module requires a pulse on PWRKEY input pin to turn on.
+	 * Pulse duration must be at least 500ms.
+	 * TODO: Move it to userspace.
+	 */
+	val = readl(LSP1_GPIO_SWPORTD_DDR);
+	val |= BIT(4);
+	writel(val, LSP1_GPIO_SWPORTD_DDR);
+
+	val = readl(LSP1_GPIO_SWPORTD_DR);
+	val &= ~BIT(4);
+	writel(val, LSP1_GPIO_SWPORTD_DR);
+
+	mdelay(500);
+
+	val = readl(LSP1_GPIO_SWPORTD_DR);
+	val |= BIT(4);
+	writel(val, LSP1_GPIO_SWPORTD_DR);
+
+	return 0;
+}
