2d4dcb2b7692f377d1a1bdd9038304bf67298b19
Andrey Khamukhin <ahamuhin@elvees.com>
2022-02-11 16:37:41 +0300

    ecam03dm_defconfig: Implement booting from MMC
    rootfs is moved to Squashfs from ext4 so Squashfs is enabled to make
    it possible to read boot script, kernel and fdt file from rootfs.
    
    Script, which corresponds for loading corresponding boot.scr depending
    on bootvol variable is provided.
    
    Issue: #IPCAMSW-1974
    Change-Id: Iaba162c09b344850e80be1c2a4cfb7868c3d5af4

--- include/configs/mcom03-ecam03.h
+++ include/configs/mcom03-ecam03.h
@@ -10,12 +10,44 @@
 
 #include "mcom03-common.h"
 
-#define BOOT_TARGET_DEVICES(func)
+#define EXTRABOOTENV "bootvol=a\0" \
+	"safe_bootvol=a\0"
+
+// TODO: Remove dtb_file_name assignment when MCOM03SW-1629 is done
+#define BOOTENV_DEV_ECAM(devtypeu, devtypel, instance) "bootcmd_ecam=" \
+	"if test \"${bootvol}\" != \"${safe_bootvol}\"; then " \
+		"if env exists tried_to_boot; then " \
+			"setenv bootvol ${safe_bootvol};" \
+			"setenv tried_to_boot;" \
+			"saveenv;" \
+		"else " \
+			"setenv tried_to_boot true;" \
+			"saveenv;" \
+		"fi;" \
+	"fi;" \
+	"test -n \"$dtb_file_name\" || dtb_file_name=mcom03-ecam03dm-r1.0.dtb;" \
+	"devtype=mmc;" \
+	"test -n \"$devnum\" || devnum=0;" \
+	"if test \"${bootvol}\" = \"a\"; then " \
+		"distro_bootpart=1;" \
+	"else " \
+		"distro_bootpart=2;" \
+	"fi;" \
+	"load ${devtype} ${devnum}:${distro_bootpart} ${scriptaddr} /boot/boot.scr; " \
+	"source ${scriptaddr}\0"
+
+#define BOOTENV_DEV_NAME_ECAM(devtypeu, devtypel, instance) "ecam "
+
+#define BOOT_TARGET_DEVICES_ECAM(func) func(ECAM, ecam, na)
+
+#define BOOT_TARGET_DEVICES(func) \
+	BOOT_TARGET_DEVICES_ECAM(func)
 
 #include <config_distro_bootcmd.h>
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	MCOM03_COMMON_ENV_SETTINGS \
-	BOOTENV
+	BOOTENV \
+	EXTRABOOTENV
 
 #endif
