b17a02050dc9111d887af77f9e6aa55cbdfec362
Oleg Ivanov <oaivanov@elvees.com>
2022-06-28 15:51:09 +0300

    board: elvees: pm03cam: Add new board support
    This defconfig is used for PM03CAM r1.0.
    
    Issue: #MCOM03SW-1660
    Change-Id: Id6948c63389690e5ae8cf1b14247325b8512142c

---
+++ board/elvees/pm03cam/pm03cam.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2022 RnD Center "ELVEES", JSC
+ */
+
+#include <common.h>
+
+#include "../common/mcom03-common.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+void board_pads_cfg(void)
+{
+	/* U-Boot doesn't have pinctrl driver, so switch pad voltage manually */
+	lsperiph1_v18_pad_cfg();
+}
