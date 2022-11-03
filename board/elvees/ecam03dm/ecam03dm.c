cb149e59d3bf589b2664388eb82be7ca1ec5f7a5
Omar Al-Wadi <oalwadi@elvees.com>
2022-11-03 15:14:24 +0300

    board: elvees: ecam03dm: Remove input enable for GPIO1a PHY's
    U-Boot is not using interrupt, so there is no need to activate
    `input-enable` for PHY's which connected to LSPERIPH1.
    
    Linux kernel must configure LSPERIPH1 pins for interrupts on its own.
    
    Issue: #MCOM03SW-1279
    Change-Id: I28b538012e31d81d41e1f34be5cb0df388d63c01

--- board/elvees/ecam03dm/ecam03dm.c
+++ board/elvees/ecam03dm/ecam03dm.c
@@ -13,17 +13,8 @@ DECLARE_GLOBAL_DATA_PTR;
 
 void board_pads_cfg(void)
 {
-	u32 val;
 	nand_pad_cfg();
 
 	/* U-Boot doesn't have pinctrl driver, so switch pad voltage manually */
 	lsperiph1_v18_pad_cfg();
-
-	/* Enable receivers for GPIO1_A6,
-	 * which are used as Ethernet PHY interrupt.
-	 * Temporary code until support is added to pinctrl
-	 */
-	val = readl(GPIO1_PORTA_PAD_CTR(6));
-	val |= GPIO_PAD_CTR_EN;
-	writel(val, GPIO1_PORTA_PAD_CTR(6));
 }
