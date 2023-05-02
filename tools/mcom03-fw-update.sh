65c5ae176dd4635ea5d74c84e09822343ef353cb
Mikhail Konovalov <mkonovalov@elvees.com>
2023-05-02 16:41:17 +0300

    tools: mcom03-fw-update: Add option "-o" to provide image offset
    Previously this script allowed to write only to zero offset and was used
    to write SBL. Now the added option allows to write any images containing
    U-Boot to specific offset, for example SBIMG images used by SBL-TL as
    part of swupdate package.
    
    Also the check of U-Boot version was modified to search string from
    provided offset.
    
    Issue: #MCOM03SW-2151
    Change-Id: I0a6afca78b272ba7b4a80df9627101a43ea87c97

--- tools/mcom03-fw-update.sh
+++ tools/mcom03-fw-update.sh
@@ -7,7 +7,7 @@ set -o pipefail
 
 function help {
     echo "Description:
-    Flash the MCom-03 SBL image to the SPI from OS running on the board.
+    Flash the MCom-03 SBL or SBIMG image to the SPI from OS running on the board.
     /etc/fw_env.config is used to detect U-Boot environment variables location.
     mtd-utils are required.
 Usage:
@@ -15,18 +15,24 @@ Usage:
 Arguments:
     IMAGE      SBL image file
 Options:
-    -h         print this message"
+    -h         print this message
+    -o         provide offset in bytes from the beginning of SPI NOR"
 }
 
 function read_uboot_version {
-    head $DEV -c 1048576 | strings | grep -e 'U-Boot [0-9]'
+    dd if=$DEV skip=$((OFFSET / BLOCK_SIZE)) bs=$BLOCK_SIZE \
+        count=$((ALIGNED_IMAGE_SIZE / BLOCK_SIZE)) 2>/dev/null | \
+        strings | grep -e 'U-Boot [0-9]'
 }
 
-while getopts "h" arg; do
+while getopts "ho:" arg; do
     case "$arg" in
     h)
         help
         exit 0
+    ;;
+    o)
+        OFFSET=$OPTARG
     ;;
     *)
         echo "Incorrect option"
@@ -53,6 +59,10 @@ if [ ! -f "$IMAGE" ]; then
     exit 1
 fi
 
+if [[ -z "$OFFSET" ]]; then
+    OFFSET=0
+fi
+
 # Get environment parameters from a non-empty and non-commented line in the
 # config file
 ENV_CONFIG=/etc/fw_env.config
@@ -71,17 +81,34 @@ BLOCK_SIZE=${BLOCK_SIZE##* }
 # shellcheck disable=SC2017
 readonly ALIGNED_IMAGE_SIZE=$(( (IMAGE_SIZE + BLOCK_SIZE - 1) / BLOCK_SIZE * BLOCK_SIZE ))
 
+# Check that offset is aligned to BLOCK_SIZE
+readonly ALIGNED_IMAGE_OFFSET=$(( (OFFSET + BLOCK_SIZE - 1) / BLOCK_SIZE * BLOCK_SIZE ))
+if [[ $OFFSET -ne $ALIGNED_IMAGE_OFFSET ]]; then
+    echo "Image offset has to be aligned to '$BLOCK_SIZE'"
+    exit 1
+fi
+
+# Check that image doesn't exceed DEV boundaries
+MTD_SIZE=$(mtd_debug info "$DEV" | grep -o "mtd.size = [0-9]*")
+MTD_SIZE=${MTD_SIZE##* }
+readonly IMAGE_END=$(( OFFSET + IMAGE_SIZE ))
+
+if [[ $IMAGE_END -gt $MTD_SIZE ]]; then
+    echo "Image exceeds $DEV boundaries writing to $OFFSET. Operation aborted."
+    exit 1
+fi
+
 echo "Flashing firmware. Do not terminate process!"
 echo "Erasing old firmware..."
 echo "U-Boot version on device: $(read_uboot_version)"
-LOG=$(mtd_debug erase "$DEV" 0 "$ALIGNED_IMAGE_SIZE" 2>&1)
+LOG=$(mtd_debug erase "$DEV" "$OFFSET" "$ALIGNED_IMAGE_SIZE" 2>&1)
 echo "Writing firmware..."
-LOG=$(mtd_debug write "$DEV" 0 "$IMAGE_SIZE" "$IMAGE" 2>&1)
+LOG=$(mtd_debug write "$DEV" "$OFFSET" "$IMAGE_SIZE" "$IMAGE" 2>&1)
 
 echo "Verifying firmware..."
 echo "U-Boot version on device: $(read_uboot_version)"
 CHECK_IMAGE=$TEMPFILES/check.image
-LOG=$(mtd_debug read "$DEV" 0 "$IMAGE_SIZE" "$CHECK_IMAGE" 2>&1)
+LOG=$(mtd_debug read "$DEV" "$OFFSET" "$IMAGE_SIZE" "$CHECK_IMAGE" 2>&1)
 if cmp "$IMAGE" "$CHECK_IMAGE" &> /dev/null; then
     echo "Flashing successful"
 else
